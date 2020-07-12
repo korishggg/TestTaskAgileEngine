02dcc7661bb59dddd46b13f3c3e463eaf2cd757a
Korish <boybkorish@gmail.com>
2020-07-12 16:02:30 +0300

    first Commit

---
+++ src/main/java/com/example/agileenginetest/AgileEngineTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.agileenginetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileEngineTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileEngineTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agileenginetest/AgileEngineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agileenginetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileEngineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
